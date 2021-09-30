73990bb583a6cb6bcf7d7caa8c857c1a92d77f23
heather kurth <hmkurth@madisoncollege.edu>
2021-09-29 22:23:19 -0500

    adding webApp directory, META_INF, and the context.xml(which will need to be updated with DB specs)

---
+++ src/main/java/controller/MainApp.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class MainApp {
+
+    public static void main(String[] args) throws Exception {
+        //SpringApplication.run(Main.class, args);
+        //TODO run without springboot
+
+    }
+
+}

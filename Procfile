24dd28ea6a63876f6e2b3d5582d77c5c5a7c3d21
heather kurth <hmkurth@madisoncollege.edu>
2021-09-29 23:59:04 -0500

    deleted context.xml, updated Procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: java -jar target/glbrc-1.0.jar
+web: sh target/bin/webapp
---
+++ src/main/java/controller/DashboardControl.java
@@ -0,0 +1,50 @@
+
+package controller;
+
+
+        import org.apache.logging.log4j.LogManager;
+        import org.apache.logging.log4j.Logger;
+
+        import javax.servlet.RequestDispatcher;
+        import javax.servlet.ServletException;
+        import javax.servlet.ServletOutputStream;
+        import javax.servlet.annotation.WebServlet;
+        import javax.servlet.http.HttpServlet;
+        import javax.servlet.http.HttpServletRequest;
+        import javax.servlet.http.HttpServletResponse;
+        import javax.servlet.http.HttpSession;
+        import java.io.IOException;
+        import java.util.List;
+
+/**
+ * This servlet will...TODO
+ */
+
+@WebServlet(
+        name = "DashboardControl",
+        urlPatterns = {"/dashboardControl"}
+)
+
+public class DashboardControl  extends HttpServlet {
+
+    private final Logger logger = LogManager.getLogger(this.getClass());
+
+
+    /**
+     * Handles HTTP GET requests.
+     *
+     * @param req the HttpServletRequest object
+     * @param resp the HttpServletResponse object
+     * @throws ServletException if there is a Servlet failure
+     * @throws IOException      if there is an IO failure
+     */
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello heroku".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
